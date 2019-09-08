d4d976b84779c5bc6d8078f380ad55ff6ab8a4ab
chico27 <masamasa.987654321@gmail.com>
2019-09-08 11:33:11 +0900

    new

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ window.onload = function() {
   let k = 1;
   for (let i = 1; i <= line; i++) {
     for (let j = 1; j <= column; j++) {
-      // selectChangeCt = Math.floor(Math.random()*10)+1
+      selectChangeCt = Math.floor(Math.random()*9)+1
       const div = document.createElement('div');
       div.id='black_rectangele_' + k;
       div.classList.add('box');
       div.classList.add('flex');
-      // div.classList.add('change_ct_' + selectChangeCt);
+      div.classList.add('having_ct_' + selectChangeCt);
+      div.classList.add('change_ct_' + selectChangeCt);
       document.body.appendChild(div);
       k++;
     }
@@ -26,15 +27,36 @@ window.onload = function() {
 function sprinkle(){
   ct = 1;
   while(ct <= maxRectangle){
-    randomNum = Math.random();
-    if (randomNum < 0.5){
-      let attr = document.getElementById("black_rectangele_" + ct);
-      if (!attr.classList.contains('box_disappear')) {
+    let attr = document.getElementById("black_rectangele_" + ct);
+    let attrClass =  attr.className;
+    let chars = attrClass.split(' ');
+    let containDisappear = false;
+
+    if (chars.includes("box_disappear")) {
+      chars.splice(chars.indexOf("box_disappear"), 1);
+      containDisappear = true;
+    }
+
+    let having_ct = chars[2].slice(-1);
+    let change_string = chars[3];
+    let change_ct = change_string.slice(-1);
+    change_ct--;
+    if (change_ct == 0){
+      // console.log(containDisappear)
+      if (!containDisappear) {
+        // console.log(true)
         attr.classList.add("box_disappear");
       } else {
+        // console.log(false)
         attr.classList.remove("box_disappear");
       }
+      change_ct = having_ct;
     }
+    attr.classList.remove(change_string)
+    attr.classList.add('change_ct_' + change_ct)
+    // if(!chars.indexOf(pattern)){
+    //   console.log(chars[string.indexOf(pattern)])
+    // }
     ct++;
   }
 }
